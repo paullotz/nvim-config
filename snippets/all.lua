7d345415f3b28fb1692f73f9eaa9aa20e1c8f94f
Paul Lotz <paul.lotz@gmx.at>
2025-09-26 20:20:22 +0200

    feat: add kanshi

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,13 +1,13 @@
-local ls = require("luasnip")
+local ls = require 'luasnip'
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
 return {
-	s("test", {
-		t("This is a test snippet for "),
-		i(1, "LaTeX"),
-		t("!"),
-	}),
-	-- Add more snippets here
+  s('test', {
+    t 'This is a test snippet for ',
+    i(1, 'LaTeX'),
+    t '!',
+  }),
+  -- Add more snippets here
 }
