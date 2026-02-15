6f586f9d1bd3e31f6540f47800069509afb8957b
Paul Lotz <paul.lotz@gmx.at>
2026-02-15 11:21:32 +0100

    feat: update dotfiles, remove mail

--- init.lua
+++ init.lua
@@ -600,7 +600,7 @@ require('lazy').setup({
 
       completion = {
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
-      },
+q      },
 
       sources = {
         default = { 'snippets', 'lsp', 'path', 'lazydev' },
--- snippets/all.lua
+++ snippets/all.lua
@@ -3,9 +3,6 @@ local s = ls.snippet
 local t = ls.text_node
 
 return {
-  s('mail', {
-    t 'paul.lotz@gmx.at',
-  }),
   s('gh', {
     t 'https://github.com/paullotz',
   }),
